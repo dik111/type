62ae0db3519f03b542111ebb4fe8d51e458680b8
yuweiZhan <195812208@qq.com>
2020-10-18 22:04:44 +0800

    :sparkles: 01-变量的基本使用

---
+++ 01-变量的基本使用.ts
@@ -0,0 +1,14 @@
+// 第一步：声明变量并指定类型
+let age:number
+
+// 第二步：给变量赋值
+age=18
+console.log(age);
+
+// 第三步：变量可以变化
+age=19
+console.log(age);
+
+// 简化方式：声明变量的同时就赋值
+let age1 :number = 20
+console.log(age1);
